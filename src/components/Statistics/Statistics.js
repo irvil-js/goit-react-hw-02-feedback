f92e68869aa6f9ffe12693fbab5d6b226d8f6f39
Irina Murashka <irvil@tut.by>
2021-09-05 23:12:32 +0100

    вынесла Notification в Components

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Notification from "./Notification";
+import Notification from "../Notification";
 import styles from "./Statistics.module.css";
 
 const Statistics = (props) => {
