54dd647114042c8771ee74cdce225f371f9a054e
Irina Murashka <irvil@tut.by>
2021-09-05 17:16:59 +0100

    счетчик отзывов

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import FeedbackCounter from "./components/Feedback/Feedback";
 
 const App = () => (
   <>
-    <FeedbackCounter />
+    <div>
+      <h1>Please leave feedback</h1>
+    </div>
+    <FeedbackCounter name="Good" />
+    <FeedbackCounter name="Neutral" />
+    <FeedbackCounter name="Bad" />
+    <div>
+      <h2>Statistics</h2>
+    </div>
   </>
 );
 
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -14,24 +14,50 @@ class FeedbackCounter extends React.Component {
     }));
   };
 
+  // handleIncrementBad = () => {
+  //   this.setState((prevState) => ({
+  //     bad: prevState.bad + 1,
+  //   }));
+  // };
+
+  // handleIncrementNeutral = () => {
+  //   this.setState((prevState) => ({
+  //     neutral: prevState.neutral + 1,
+  //   }));
+  // };
+
+  // SumFeedback = () => {
+  //   this.setState((prevState) => ({
+  //     total: prevState.good + prevState.neutral + prevState.bad,
+  //   }));
+  // };
+
   render() {
     return (
       <div>
-        <h1>Please leave feedback</h1>
         <div>
           <button type="button" onClick={this.handleIncrement}>
-            Good
+            {this.props.name}
           </button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          {/* <button type="button" onClick={this.handleIncrementNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleIncrementBad}>
+            Bad
+          </button> */}
         </div>
-        <h2>Statistics</h2>
+        {/* <h2>Statistics</h2> */}
         <div>
-          <span className="FeedbackCounter_value">Good: {this.state.good}</span>
           <span className="FeedbackCounter_value">
+            {this.props.name}: {this.state.good}
+          </span>
+          {/* <span className="FeedbackCounter_value">
             Neutral: {this.state.neutral}
           </span>
-          <span className="FeedbackCounter_value">Bad: {this.state.bad}</span>
+          <span className="FeedbackCounter_value">Bad: {this.state.bad}</span> */}
+          {/* <span className="FeedbackTotal" onChange={this.SumFeedback}>
+            Total:{" "} */}
+          {/* </span> */}
         </div>
       </div>
     );
